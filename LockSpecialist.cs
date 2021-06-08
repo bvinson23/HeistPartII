16f58a2a8114c830f8d3a6fd7df4126da3b35013
bvinson23 <brandonvinson23@gmail.com>
2021-06-08 13:18:41 -0500

    LockSpecialist class created

---
+++ LockSpecialist.cs
@@ -0,0 +1,22 @@
+using System;
+
+namespace HeistPartII
+{
+    public class LockSpecialist : IRobber
+    {
+        public string Name { get; set; }
+        public int SkillLevel { get; set; }
+        public int PercentageCut { get; set; }
+        public void PerformSkill(Bank bank)
+        {
+            int skill = bank.VaultScore - SkillLevel;
+
+            Console.WriteLine($"{Name} is picking the vault lock. Decreased lock {SkillLevel} points.");
+
+            if (bank.VaultScore <= 0)
+            {
+                Console.WriteLine($"{Name} has picked the lock!");
+            }
+        }
+    }
+}
