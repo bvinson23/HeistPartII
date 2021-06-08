e4c05df5946d6a33417061c4068fd118a776ac36
bvinson23 <brandonvinson23@gmail.com>
2021-06-08 14:39:16 -0500

    user can create a robber

--- Program.cs
+++ Program.cs
@@ -7,6 +7,10 @@ namespace HeistPartII
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("-----------------------------");
+            Console.WriteLine("| Welcome to Heist Builder. |");
+            Console.WriteLine("-----------------------------");
+
             LockSpecialist jake = new LockSpecialist()
             {
                 Name = "Jake Peralta",
@@ -50,17 +54,71 @@ namespace HeistPartII
             };
 
             List<IRobber> rolodex = new List<IRobber>()
+                {
+                    jake, pontiac, amy, boyle, rosa, terry
+                };
+
+            void Create()
             {
-                jake, pontiac, amy, boyle, rosa, terry
-            };
+                Console.WriteLine($"There are {rolodex.Count} robbers in the rolodex.");
+                Console.WriteLine();
 
-            Console.WriteLine("-----------------------------");
-            Console.WriteLine("| Welcome to Heist Builder. |");
-            Console.WriteLine("-----------------------------");
+                Console.WriteLine("Create a new crew member.");
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
 
-            Console.WriteLine($"There are {rolodex.Count} robbers in the rolodex.");
+                if (name.Length > 0)
+                {
+                    Console.WriteLine(@"Choose a specialty:
+                1) Hacker (Disables alarms)
+                2) Muscle (Disarms guards)
+                3) Lock Specialist (Cracks vault) ");
+                    int specialty = Int32.Parse(Console.ReadLine());
 
+                    Console.Write($"Enter {name}'s Skill Level (1-100): ");
+                    int skill = Int32.Parse(Console.ReadLine());
 
+                    Console.Write($"Enter {name}'s cut of the loot (1-100 %): ");
+                    int cut = Int32.Parse(Console.ReadLine());
+
+                    if (specialty == 1)
+                    {
+                        Hacker hacker = new Hacker()
+                        {
+                            Name = name,
+                            SkillLevel = skill,
+                            PercentageCut = cut
+                        };
+                        rolodex.Add(hacker);
+                    }
+                    else if (specialty == 2)
+                    {
+                        Muscle muscle = new Muscle()
+                        {
+                            Name = name,
+                            SkillLevel = skill,
+                            PercentageCut = cut
+                        };
+                        rolodex.Add(muscle);
+                    }
+                    else if (specialty == 3)
+                    {
+                        LockSpecialist lockSpecialist = new LockSpecialist()
+                        {
+                            Name = name,
+                            SkillLevel = skill,
+                            PercentageCut = cut
+                        };
+                        rolodex.Add(lockSpecialist);
+                    }
+                    Create();
+                }
+                else
+                {
+                    Console.WriteLine("Time for the heist!!");
+                }
+            }
+            Create();
         }
     }
 }
